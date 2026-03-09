1df6d63c5ebad58d8a84e3dfbfd0a42db0ce167c
lichen <lichenwang0114@gmail.com>
2026-03-09 15:55:47 +0800

    feat: add IsLeapYear class to determine if a year is a leap year.

---
+++ 2/IsLeapYear.java
@@ -0,0 +1,14 @@
+public class IsLeapYear {
+
+    /**
+     * Determines whether a given year is a leap year.
+     * * @param year an integer representing the year (1 <= year <= 10000)
+     * 
+     * @return true if the year is a leap year, false otherwise
+     */
+    public boolean determine(int year) {
+        // A year is a leap year if it is divisible by 400,
+        // OR if it is divisible by 4 but NOT by 100.
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+}
